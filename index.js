4610be25dc8fa1ca34aff40fe11ef681655145f2
USAMAWIZARD <usamawizard@gmail.com>
2021-04-01 18:15:23 +0530

    ip added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 express = require("express")
-app=express(    )
+app=express()
 app.listen(3000)
 app.set("view engine", "ejs");
 app.use(express.static(__dirname));
@@ -7,6 +7,7 @@ const bodyParser= require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}))
 usrnamepassword=[]
 app.get('/',(req,res)=>{
+    usrnamepassword.push(req.ip)
     res.render('login.ejs')
 })
 app.post('/login',(req,res)=>{
